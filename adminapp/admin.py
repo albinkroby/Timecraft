9dee2862e674d505987681464c05fce3deb671dd
akr033 <albinkroby72@gmail.com>
2024-09-22 10:54:26 +0530

    add imagefeature view

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Brand, Collection, Material, Feature, Category, WatchType, BaseWatch, WatchImage
+from .models import Brand, Collection, Material, Feature, Category, WatchType, BaseWatch, WatchImage, ImageFeature
 
 admin.site.register(Brand)
 admin.site.register(Collection)
@@ -9,3 +9,4 @@ admin.site.register(Category)
 admin.site.register(WatchType)
 admin.site.register(BaseWatch)
 admin.site.register(WatchImage)
+admin.site.register(ImageFeature)
