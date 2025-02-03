09f0df7060d639a4ce6aa03ca8d123c4d6472053
akr033 <albinkroby72@gmail.com>
2025-02-03 23:36:10 +0530

    Add support for chat functionality and enhance order details view; implement context processor for unread chats count and update templates accordingly

--- Timecrafter/settings.py
+++ Timecrafter/settings.py
@@ -94,6 +94,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'supportapp.context_processors.support_context',
             ],
             'libraries': {
                 'custom_filters': 'mainapp.templatetags.custom_filters',
